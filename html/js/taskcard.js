7f8f06150362dbd5fe00acfb3bc7ef95e421818e
Simon Fröhner <simon@wk-froehner.de>
2020-09-19 20:36:04 +0200

    Added dragdrop to kanban.html and folder structure

---
+++ html/js/taskcard.js
@@ -0,0 +1,65 @@
+// Dragdrop Tasks
+// Compare to "https://developer.mozilla.org/en-US/docs/Web/API/Document/drag_event"
+
+let dragged;
+
+document.addEventListener("drag", function(event) {
+
+}), false;
+
+// On drag start
+document.addEventListener("dragstart", function(event) {
+    // referenece the dragged element on drag start
+    dragged = event.target;
+}, false);
+
+// On drag end
+document.addEventListener("dragend", function(event) {
+
+}, false);
+
+// On drag over
+document.addEventListener("dragover", function(event) {
+    event.preventDefault();
+}, false);
+
+// On entering the dropzone
+document.addEventListener("dragenter", function(event) {
+    // highlight target
+    if(event.target.className == "column dropzone") {
+        event.target.style.background = "rgb(160, 114, 83)";
+    }
+}, false);
+
+// On leaving the dropzone
+document.addEventListener("dragleave", function(event) {
+    // reset highlight on target
+    if(event.target.className == "column dropzone") {
+        event.target.style.background = "";
+    }
+}, false);
+
+// On drop
+document.addEventListener("drop", function(event) {
+    // prevent default action
+    event.preventDefault();
+    
+    // move the dragged element to the dropzone
+    if(event.target.className == "column dropzone") {
+        event.target.style.background = "";
+
+        // Select the "Create a new task.."-node
+        let lastChild  = event.target.lastChild.previousSibling;
+
+        // Remove element from former parent
+        dragged.parentNode.removeChild(dragged);      
+        // Add element as child of target
+        event.target.appendChild(dragged);
+
+        // Place the "Create a new task.."-node as last element
+        lastChild.parentNode.removeChild(lastChild);
+        event.target.appendChild(lastChild);
+    }
+
+}, false);
+
