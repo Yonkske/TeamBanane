08885ae510239b9f3c2decd1fcd73c5e64a7e70f
Simon Fröhner <simon@wk-froehner.de>
2020-09-20 11:28:46 +0200

    List sortable now

--- html/js/taskcard.js
+++ html/js/taskcard.js
@@ -11,6 +11,11 @@ document.addEventListener("drag", function(event) {
 document.addEventListener("dragstart", function(event) {
     // referenece the dragged element on drag start
     dragged = event.target;
+
+    // For making it sortable
+    if(event.target.className == "task-card") {
+        dragStart(event);
+    }
 }, false);
 
 // On drag end
@@ -21,6 +26,11 @@ document.addEventListener("dragend", function(event) {
 // On drag over
 document.addEventListener("dragover", function(event) {
     event.preventDefault();
+
+    // For making it sortable
+    if(event.target.className == "task-card") {
+        dragOver(event);
+    }
 }, false);
 
 // On entering the dropzone
@@ -62,3 +72,34 @@ document.addEventListener("drop", function(event) {
     }
 
 }, false);
+
+// Functions to make the task-cards orderable
+// Compare to https://stackoverflow.com/questions/10588607/tutorial-for-html5-dragdrop-sortable-list
+
+let el;
+
+function dragOver(event) {
+    if (isBefore(el, event.target)){
+        event.target.parentNode.insertBefore(el, event.target);
+    }
+    else {
+        event.target.parentNode.insertBefore(el, event.target.nextSibling);
+    }
+};
+
+function dragStart(event) {
+    event.dataTransfer.effectAllowed = "move";
+    event.dataTransfer.setData("text/plain", null); // Thanks to bqlou for their comment.
+    el = event.target;
+};
+
+function isBefore(el1, el2) {
+    if (el2.parentNode === el1.parentNode) {
+        for (var cur = el1.previousSibling; cur && cur.nodeType !== 9; cur = cur.previousSibling) {
+            if (cur === el2) {
+                return true;
+            }
+        }
+    }
+  return false;
+};
